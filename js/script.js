adfc9ce6752db50ba3158f8e7b72a1379ec2d4d4
Achmad Yama Roni S <yamaroni8@gmail.com>
2024-08-14 20:06:27 +0700

    update js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
 // Ini Javascript
+
+console.log('linked success');
+
+function formValidate() {
+    let inputWeight = document.getElementById('input-weight').value;
+    let inputHeight = document.getElementById('input-height').value;
+    console.log(`isi inputan berat badan: ${inputWeight}`);
+    console.log(`isi inputan tinggi badan: ${inputHeight}`);
+
+    (inputWeight == '' || inputHeight == '')
+        ? alert('Inputan Berat Badan Kosong!')
+        : result(inputHeight, inputWeight);
+    console.log('form submitted');
+}
+
+function result(inputHeight, inputWeight) {
+    let convertCmToM = inputHeight * 100;
+    let resultHeight = Math.pow(convertCmToM, 2);
+    console.log(inputWeight / resultHeight);
+}
